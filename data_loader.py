849c099215606e9031c23b18c55e2531811d1405
bmajosek <bartek9568@gmail.com>
2024-11-17 17:33:20 +0100

    repair main

--- data_loader.py
+++ data_loader.py
@@ -2,30 +2,28 @@ import pandas as pd
 from preprocessing import encode_labels
 
 def load_wine_data():
-    df = pd.read_csv('/content/drive/MyDrive/ml/WineQT.csv')
+    df = pd.read_csv('datasets/WineQT.csv')
     y = df['quality']
     X = df.drop(columns='quality')
     y_encoded = encode_labels(y)
     return X, y_encoded
 
 def load_drug_data():
-    df = pd.read_csv('/content/drive/MyDrive/ml/drug.csv')
+    df = pd.read_csv('datasets/drug.csv')
     X = df.drop(columns='Drug')
     y = df['Drug']
     y_encoded = encode_labels(y)
     return X, y_encoded
 
 def load_iris_data():
-    df = pd.read_csv('/content/drive/MyDrive/ml/Iris.csv')
+    df = pd.read_csv('datasets/Iris.csv')
     y = df["Species"]
     y_encoded = encode_labels(y)
     X = df.drop(columns=["Species", "Id"])
     return X, y_encoded
 
 def load_titanic_data():
-    train = pd.read_csv('/content/drive/MyDrive/ml/train.csv')
+    train = pd.read_csv('datasets/train.csv')
     X_train = train.drop(columns=["Survived", "PassengerId"])
     y_train = train["Survived"]
-    X_test = pd.read_csv('/content/drive/MyDrive/ml/test.csv').sort_values(by=["PassengerId"]).drop(columns=["PassengerId"])
-    y_test = pd.read_csv('/content/drive/MyDrive/ml/gender_submission.csv').sort_values(by=["PassengerId"])["Survived"]
-    return X_train, y_train, X_test, y_test
+    return X_train, y_train
